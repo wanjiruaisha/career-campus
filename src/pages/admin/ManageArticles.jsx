2c6235d6ab90387b39f5f3f85dfd949ddc987c26
wanjiruaisha <wanjiruaisha30@gmail.com>
2026-07-15 18:17:47 +0300

    feat: add initial components for AddArticle, Dashboard, EditArticle, and ManageArticles pages

--- src/pages/admin/ManageArticles.jsx
+++ src/pages/admin/ManageArticles.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Dashboard() {
+function Articles() {
   return (
     <div>
       
@@ -8,4 +8,4 @@ function Dashboard() {
   )
 }
 
-export default Dashboard
+export default Articles
