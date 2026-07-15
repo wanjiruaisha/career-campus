009101f6b1e51611fd64e5b0576075cb48997151
wanjiruaisha <wanjiruaisha30@gmail.com>
2026-07-15 18:17:57 +0300

    feat: add AuthListener and Toaster components to main application entry

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { Toaster } from "@/components/ui/sonner";
 
 import App from "./App";
 import "./index.css";
 
 import { store } from "./store/store";
 import { ThemeProvider } from "./components/ThemeProvider";
+import AuthListener from "./components/AuthListener";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider>
         <BrowserRouter>
+          <AuthListener />
           <App />
+          <Toaster richColors position="top-right" />
         </BrowserRouter>
       </ThemeProvider>
     </Provider>
